be200015e19bc0e7aa24a972fd3444886603b245
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-17 12:31:40 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -20,7 +20,8 @@ class Timer extends Component {
   }
   
   componentDidUpdate(){
-    
+    this.timer.current.style.color =
+  "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 
   componentWillUnmount() {
